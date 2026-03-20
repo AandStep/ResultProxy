da028a93e5fb1337d71a08155f72b917ec7f55ad
Елизавета Максимова <andbet18@gmail.com>
2026-03-20 17:10:43 +0700

    feat(ui): dynamic version parsing from package.json

--- vite.config.js
+++ vite.config.js
@@ -17,10 +17,16 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+  },
   // ОЧЕНЬ ВАЖНО: Указываем относительный базовый путь для корректной работы в Electron (file://)
   base: "./",
 });
